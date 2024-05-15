2811b9319aeb01011aad72e3a96ff24c14c59232
hsinyun <ruby8941@gmail.com>
2024-05-16 01:17:00 +0800

    test

--- dist/service/auth.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-//generateJwt
-//isAuth
--- dist/service/handleErrorAsync.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const handleErrorAsync = (fun) => {
-    return function (req, res, next) {
-        return fun(req, res, next).catch((err) => next(err));
-    };
-};
-exports.default = handleErrorAsync;
