6808cd0c81c9cc8953a2c66d5db96b51ece5844e
馨云 <hsinyun@xinyundeMacBook-Pro.local>
2024-05-27 14:34:42 +0800

    merge

--- app.ts
+++ app.ts
@@ -1,39 +1,34 @@
-import express from "express";
-import path from "path";
-import cookieParser from "cookie-parser";
-import logger from "morgan";
-import mongoose from "mongoose";
+import express from 'express';
+import path from 'path';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+import mongoose from 'mongoose';
 import { Request, Response, NextFunction } from 'express';
-import AppError from "./types/AppError";
+import AppError from './types/AppError';
 import appErrorService from './service/appErrorService';
-import {resErrorProd, resErrorDev} from './service/resError';
+import { resErrorProd, resErrorDev } from './service/resError';
 import swaggerUI from 'swagger-ui-express';
+import apiLimiter from './service/rateLimit';
+//router
+import usersRouter from './routes/users';
+//env
+import dotenv from 'dotenv';
+dotenv.config({ path: './.env' });
 
 const rootDir = process.cwd();
 const swaggerFilePath = path.join(rootDir, 'swagger-output.json');
 const swaggerFile = require(swaggerFilePath);
 
-//env
-import dotenv from 'dotenv'; 
-dotenv.config({ path: './.env' });
-
 //mongoose
-// const DB = process.env.DATABASE!.replace(
-//   '<password>',
-//   process.env.DATABASE_PASSWORD!,
-// );
-const DB = process.env.MONGO_CONNECTION_STRING!;
+const DB = process.env.DATABASE!.replace('<password>', process.env.DATABASE_PASSWORD!);
 mongoose
   .connect(DB)
   .then(() => {
     console.log('資料庫連線成功');
   })
-  .catch((err:Error) => {
+  .catch((err: Error) => {
     console.log(err, '資料庫連線異常');
   });
-
-//router
-import usersRouter from './routes/users';
 
 const app = express();
 
@@ -43,15 +38,17 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//api rate limit
+app.use('/api', apiLimiter);
 //route
 app.use('/', usersRouter);
-app.use('/api-doc', swaggerUI.serve, swaggerUI.setup(swaggerFile))
+app.use('/api-doc', swaggerUI.serve, swaggerUI.setup(swaggerFile));
 //404
 app.use(function (req: Request, res: Response, next: NextFunction) {
   appErrorService(404, '找不到路徑', next);
 });
 //error
-app.use(function (err: AppError, req: Request, res: Response,next:NextFunction) {
+app.use(function (err: AppError, req: Request, res: Response, next: NextFunction) {
   err.statusCode = err.statusCode || 500;
   if (process.env.NODE_ENV === 'dev') {
     return resErrorDev(err, res);
--- service/auth.ts
+++ service/auth.ts
@@ -1,2 +1,59 @@
-//generateJwt
+import appErrorService from './appErrorService';
+import type { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+import handleSuccess from './handleSuccess';
+import OauthAccessToken from '../models/oauthAccessToken';
+import type { ObjectId } from 'mongodb';
+//checkUserExist
+//create oauthAccessToken
+export const createToken = async (userId: ObjectId, day: number) => {
+  const accessToken = await OauthAccessToken.create({
+    user: userId,
+    name: 'PersonalAccessToken',
+    expiresAt: new Date(Date.now() + 3600 * day * 1000),
+  });
+  return accessToken;
+};
+//generateJwtSend
+export const generateJwtSend = async (userId: ObjectId, res: Response) => {
+  const secret = process.env.JWT_SECRET!;
+  const expiresDay = process.env.JWT_EXPIRES_DAY!;
+
+  const dayNum = extractDays(expiresDay);
+  const oauthToken = await createToken(userId, dayNum);
+
+  const jwtData = {
+    id: userId,
+    oauthTokenId: oauthToken.id,
+  };
+  const token = jwt.sign(jwtData, secret, {
+    expiresIn: expiresDay,
+  });
+  const data = {
+    token,
+  };
+  handleSuccess(res, 200, '', data);
+};
+
+const extractDays = (expiresIn: string) => {
+  const match = expiresIn.match(/(\d+)d/);
+  return match ? parseInt(match[1], 10) : 7;
+};
 //isAuth
+export const isAuth = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    token = req.headers.authorization.split(' ')[1];
+  }
+
+  if (!token) {
+    return appErrorService(401, 'unauthenticated', next);
+  }
+};
