077092b279b3b4701ab3906c8f7f3a5588d272ed
hsinyun <ruby8941@gmail.com>
2024-05-29 23:14:14 +0800

    test

--- routes/users.ts
+++ routes/users.ts
@@ -18,10 +18,10 @@ const router = express.Router();
 
 //user signin,signup
 router.post('/api/auth/register', register); // 註冊學員
-router.post('/api/auth/sign-in', throttle, signIn); // 登入
-router.post('/api/auth/verify-email', throttle, sendVerifyEmail); // 寄送Email驗證信
+// router.post('/api/auth/sign-in', throttle, signIn); // 登入
+// router.post('/api/auth/verify-email', throttle, sendVerifyEmail); // 寄送Email驗證信
 router.get('/api/auth/email-link/:userId', signedMiddleware, verifyEmail); // 驗證Email
-router.post('/api/auth/forget-password', throttle, sendForgetPassword); // 忘記密碼
+// router.post('/api/auth/forget-password', throttle, sendForgetPassword); // 忘記密碼
 router.post('/api/auth/reset-password/:userId', signedMiddleware, resetPassword); // 重設密碼
 router.post('/api/auth/update-password', isAuth, updatePassword); // 更新密碼
 router.get('/api/auth/user-info', isAuth, userInfo); // 取得使用者資訊
