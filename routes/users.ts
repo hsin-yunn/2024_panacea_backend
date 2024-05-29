589d73d86447a6419cb68f893c7814d5a96753a0
hsinyun <ruby8941@gmail.com>
2024-05-30 01:18:04 +0800

    test

--- routes/users.ts
+++ routes/users.ts
@@ -1,8 +1,6 @@
 import express from 'express';
 import {
-  applyCoach,
   userInfo,
-  userUpdate,
   updatePassword,
 } from '../controllers/users';
 const router = express.Router();
@@ -10,7 +8,5 @@ const router = express.Router();
 //user signin,signup
 router.post('/api/auth/update-password', updatePassword); // 更新密碼
 router.get('/api/auth/user-info', userInfo); // 取得使用者資訊
-router.patch('/api/auth/update-user', userUpdate); // 更新使用者資訊
-router.post('/api/auth/apply-coach', applyCoach); // 註冊教練
 
 export default router;
