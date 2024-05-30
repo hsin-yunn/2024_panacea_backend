6c739f30de0dfe28a7c5d2e81ef88f670ebe1ec3
馨云 <hsinyun@xinyundeMacBook-Pro.local>
2024-05-30 14:10:30 +0800

    test

--- routes/users.ts
+++ routes/users.ts
@@ -1,6 +1,5 @@
 import express from 'express';
 import userController from '../controllers/users';
-import { isAuth } from '../service/auth';
 const router = express.Router();
 
 //user signin,signup
@@ -8,9 +7,5 @@ router.post('/api/auth/register', userController.register); // 註冊學員
 router.post('/api/auth/sign-in', userController.signIn); // 登入
 router.post('/api/auth/verify-email', userController.sendVerifyEmail); // 寄送Email驗證信
 router.post('/api/auth/forget-password', userController.sendForgetPassword); // 忘記密碼
-router.post('/api/auth/update-password', isAuth, userController.updatePassword); // 更新密碼
-router.get('/api/auth/user-info', isAuth, userController.userInfo); // 取得使用者資訊
-router.patch('/api/auth/update-user', isAuth, userController.userUpdate); // 更新使用者資訊
-router.post('/api/auth/apply-coach', isAuth, userController.applyCoach); // 註冊教練
 
 export default router;
