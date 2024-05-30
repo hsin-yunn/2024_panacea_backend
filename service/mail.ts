2db3186297d21fcdd3f4f6fea3d477e6e979f73c
hsinyun <ruby8941@gmail.com>
2024-05-30 23:46:45 +0800

    test

--- service/mail.ts
+++ service/mail.ts
@@ -5,7 +5,7 @@ import { google } from 'googleapis';
 // const nodemailerHandlebars = require('nodemailer-express-handlebars');
 
 export const registerMailSend = async (email: string, userId: string, res: Response) => {
-  const OAuth2 = google.auth.OAuth2;
+  // const OAuth2 = google.auth.OAuth2;
   const mailSender = process.env.MAIL_SENDER;
   const verifyUrl = `/api/auth/email-link/${userId}`;
 
