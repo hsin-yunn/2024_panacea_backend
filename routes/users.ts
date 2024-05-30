3b56f0ffafca011c6539e177058ee73c495bbe4c
馨云 <hsinyun@xinyundeMacBook-Pro.local>
2024-05-30 14:14:45 +0800

    test

--- routes/users.ts
+++ routes/users.ts
@@ -5,7 +5,5 @@ const router = express.Router();
 //user signin,signup
 router.post('/api/auth/register', userController.register); // 註冊學員
 router.post('/api/auth/sign-in', userController.signIn); // 登入
-router.post('/api/auth/verify-email', userController.sendVerifyEmail); // 寄送Email驗證信
-router.post('/api/auth/forget-password', userController.sendForgetPassword); // 忘記密碼
 
 export default router;
