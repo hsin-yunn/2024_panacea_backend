6e61d7897dcecf11d29c0f7f99e5189676604dd7
hsinyun <ruby8941@gmail.com>
2024-05-30 23:34:54 +0800

    fix

--- service/mail.ts
+++ service/mail.ts
@@ -1,10 +1,11 @@
 import { Response } from 'express';
+import { google } from 'googleapis';
 // const exphbs = require('express-handlebars');
 
 // const nodemailerHandlebars = require('nodemailer-express-handlebars');
 
 export const registerMailSend = async (email: string, userId: string, res: Response) => {
-  // const OAuth2 = google.auth.OAuth2;
+  const OAuth2 = google.auth.OAuth2;
   const mailSender = process.env.MAIL_SENDER;
   const verifyUrl = `/api/auth/email-link/${userId}`;
 
