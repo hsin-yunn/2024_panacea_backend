b6efe7087f82123cf82955cced72e58104e7ea93
馨云 <hsinyun@xinyundeMacBook-Pro.local>
2024-05-30 18:06:24 +0800

    fix

--- service/mail.ts
+++ service/mail.ts
@@ -11,57 +11,37 @@ const OAuth2 = google.auth.OAuth2;
 const mailSender = process.env.MAIL_SENDER;
 
 export const registerMailSend = async (email: string, userId: string, res: Response) => {
-  //webUrl 改成前端的 pageUrl 加上 temporaryUrl 的 expires,signature,userId
   const verifyUrl = `/api/auth/email-link/${userId}`;
-  const signatureOb = temporarySignature(verifyUrl, 60, { userId: userId });
-
-  const { expires, signature } = signatureOb;
-  const webUrl = `${process.env.FRONTEND_URL}/login/${userId}?expires=${expires}&signature=${signature}`;
-
-  let mailOptions = {
-    from: mailSender,
-    to: email,
-    subject: '[Panacea] 驗證信箱',
-    template: 'emailTemplate',
-    context: {
-      title: 'title',
-      header: '親愛的使用者您好，感謝您註冊 Panacea！請點擊以下按鈕來驗證您的電子郵件地址，以完成您的註冊：',
-      content: '如果您未註冊 Panacea，請忽略此郵件。此連結將在1小時後過期。',
-      buttonLink: webUrl,
-      buttonText: '驗證信箱',
-    },
-  };
-
-  const GOOGLE_AUTH_CLIENTID = process.env.GOOGLE_AUTH_CLIENTID;
-  const GOOGLE_AUTH_CLIENT_SECRET = process.env.GOOGLE_AUTH_CLIENT_SECRET;
-  const GOOGLE_AUTH_REFRESH_TOKEN = process.env.GOOGLE_AUTH_REFRESH_TOKEN;
-
   const oauth2Client = new OAuth2(
-    GOOGLE_AUTH_CLIENTID,
-    GOOGLE_AUTH_CLIENT_SECRET,
-    'https://developers.google.com/oauthplayground'
-  );
+    process.env.GOOGLE_AUTH_CLIENTID, 
+    process.env.GOOGLE_AUTH_CLIENT_SECRET, 
+    "https://developers.google.com/oauthplayground" 
+);
 
-  oauth2Client.setCredentials({
-    refresh_token: GOOGLE_AUTH_REFRESH_TOKEN,
-  });
+oauth2Client.setCredentials({
+    refresh_token: process.env.GOOGLE_AUTH_REFRESH_TOKEN
+});
 
-  const accessToken = oauth2Client.getAccessToken();
+const accessToken = oauth2Client.getAccessToken()
+let transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+      type: "OAuth2",
+      user: "gonsakon@gmail.com", 
+      clientId: process.env.GOOGLE_AUTH_CLIENTID,
+      clientSecret: process.env.GOOGLE_AUTH_CLIENT_SECRET,
+      refreshToken: process.env.GOOGLE_AUTH_REFRESH_TOKEN,
+      accessToken: accessToken
+  }
+});
 
-  let transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      type: 'OAuth2',
-      user: mailSender,
-      clientId: GOOGLE_AUTH_CLIENTID,
-      clientSecret: GOOGLE_AUTH_CLIENT_SECRET,
-      refreshToken: GOOGLE_AUTH_REFRESH_TOKEN,
-      accessToken: accessToken,
-    },
-  } as nodemailer.TransportOptions);
-
-  await transporter.sendMail(mailOptions);
-
-  handleSuccess(res, 200, 'email is sent');
+const mailOptions = {
+  from: '廖洧杰 <gonsakon@gmail.com>',
+};
+await transporter.sendMail(mailOptions);
+res.status(200).json({
+  status: 'success',
+  message: "信件發送成功"
+});
 };
 
