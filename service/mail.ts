92f9a4b077445d4b66f2dfebefb90341824e85e7
馨云 <hsinyun@xinyundeMacBook-Pro.local>
2024-05-30 17:24:43 +0800

    fix

--- service/mail.ts
+++ service/mail.ts
@@ -11,46 +11,7 @@ const OAuth2 = google.auth.OAuth2;
 const mailSender = process.env.MAIL_SENDER;
 
 export const sendMail = async (options: object, res: Response) => {
-  const oauth2Client = new OAuth2(
-    process.env.GOOGLE_AUTH_CLIENTID, 
-    process.env.GOOGLE_AUTH_CLIENT_SECRET, 
-    "https://developers.google.com/oauthplayground" 
-);
-
-oauth2Client.setCredentials({
-  refresh_token: process.env.GOOGLE_AUTH_REFRESH_TOKEN
-});
-
-  const accessToken = oauth2Client.getAccessToken();
-
-  let transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      type: 'OAuth2',
-      user: mailSender,
-      clientId: process.env.GOOGLE_AUTH_CLIENTID,
-            clientSecret: process.env.GOOGLE_AUTH_CLIENT_SECRET,
-            refreshToken: process.env.GOOGLE_AUTH_REFRESH_TOKEN,
-      accessToken: accessToken,
-    },
-  } as nodemailer.TransportOptions);
-
-  // const handlebarsOptions = {
-  //   viewEngine: {
-  //     extName: '.handlebars',
-  //     partialsDir: path.join(__dirname, '..', 'templates'),
-  //     layoutsDir: path.join(__dirname, '..', 'templates'),
-  //     defaultLayout: 'emailTemplate.handlebars',
-  //   },
-  //   viewPath: path.join(__dirname, '..', 'templates'),
-  //   extName: '.handlebars',
-  // };
-
-  // transporter.use('compile', nodemailerHandlebars(handlebarsOptions));
-
-  await transporter.sendMail(options);
-
-  handleSuccess(res, 200, 'email is sent');
+  
 };
 
 export const registerMailSend = async (email: string, userId: string, res: Response) => {
