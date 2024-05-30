75e21cdede455d2d0fd2a42fcd2cf94aa1e48a2a
馨云 <hsinyun@xinyundeMacBook-Pro.local>
2024-05-30 15:28:38 +0800

    test

--- routes/users.ts
+++ routes/users.ts
@@ -1,8 +1,6 @@
 import express from 'express';
-import userController from '../controllers/users';
 const router = express.Router();
 
 //user signin,signup
-router.post('/api/auth/sign-in', userController.signIn); // 登入
 
 export default router;
